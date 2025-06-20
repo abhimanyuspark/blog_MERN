9ee8e8acfae2dea02081328bd6ec2aa5f13cd139
abhimanyuspark <127104226+abhimanyuspark@users.noreply.github.com>
2025-06-20 22:52:12 +0530

    feat: update Loader component styles and adjust Login page layout; remove unused Loader import

--- frontend/src/components/loaders/Loader.jsx
+++ frontend/src/components/loaders/Loader.jsx
@@ -1,6 +1,6 @@
 const Loader = () => {
   return (
-    <div className="absolute size-full top-0 left-0 flex items-center justify-center">
+    <div className="absolute size-full top-0 left-0 z-50 flex items-center justify-center bg-black opacity-50">
       <span className="loading loading-spinner loading-xl"></span>
     </div>
   );
