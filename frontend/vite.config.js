f78526a661dd7810b6c26f20cc0fe6dc4bb34668
abhimanyuspark <127104226+abhimanyuspark@users.noreply.github.com>
2025-07-21 18:13:35 +0530

    z

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,13 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  define: {
+    global: {},
+    require: undefined, // Prevent "require is not defined" error
+  },
+  resolve: {
+    alias: {
+      quill: "quill/dist/quill.js",
+    },
+  },
 });
